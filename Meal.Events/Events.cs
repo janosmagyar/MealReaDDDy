e5800a108cef9e8c7fed8e36dc5ef85b78fa7dcc
János Magyar <37937364+janosmagyar@users.noreply.github.com>
2023-02-06 18:33:52 +0100

    First Feature: Ordering

--- Meal.Events/Events.cs
+++ Meal.Events/Events.cs
@@ -1,6 +1,19 @@
+using EventStore.Api;
+
 namespace Meal.Events
 {
-    public class Events
+    public record MealOrdered : Event
     {
+        public required int OrderNumber { get; init; }
+        public required int? Table { get; init; }
+        public required string Serving { get; init; }
+        public required Item[] Items { get; init; }
+    }
+
+    public record Item
+    {
+        public required int Count { get; init; }
+        public required string Name { get; init; }
+        public required string Category { get; init; }
     }
 }
--- Meal.Ordering/Meal.cs
+++ Meal.Ordering/Meal.cs
@@ -1,4 +1,5 @@
 using EventStore.Api;
+using Meal.Events;
 using Meal.Ordering.Api;
 
 namespace Meal.Ordering;
@@ -7,15 +8,75 @@ public class Meal
 {
     private readonly IEventStore _eventStore;
     private readonly string _id;
-    public MealPublicState MealPublicState => new();
+    private readonly OrderNumber _orderNumber;
+    private readonly TableNumber _tableNumber;
+    private readonly Serving _serving;
+    private readonly TrackedItem[] _items;
+    private readonly OrderState _state;
+    private readonly PaymentState _paymentState;
+
+    public MealPublicState MealPublicState => new()
+    {
+        OrderNumber = _orderNumber,
+        Payment = _paymentState,
+        Serving = _serving,
+        Table = _tableNumber,
+        State = _state,
+        Items = _items
+            .Select(i => new OrderedItem(i.Count, i.Name, i.Category, i.IsPrepared))
+            .ToArray()
+    };
 
     public Meal(IEventStore eventStore, string id)
     {
         _eventStore = eventStore;
         _id = id;
+        foreach (var persistedEvent in eventStore.Events(id))
+        {
+            switch (persistedEvent.Event)
+            {
+                case MealOrdered mealOrdered:
+                    _orderNumber = mealOrdered.OrderNumber;
+                    _tableNumber = mealOrdered.Table;
+                    _serving = Enum.Parse<Serving>(mealOrdered.Serving);
+                    _items = mealOrdered.Items
+                        .Select(i => new TrackedItem(
+                            i.Count,
+                            i.Name,
+                            Enum.Parse<Category>(i.Category)))
+                        .ToArray();
+
+                    _state = OrderState.InPreparation;
+                    _paymentState = PaymentState.Waiting;
+                    break;
+            }
+        }
     }
 
-    public void Order(OrderMealCommand command, IOrderNumberGenerator orderNumberGenerator) { }
+    public void Order(OrderMealCommand command, IOrderNumberGenerator orderNumberGenerator)
+    {
+        if (_state != OrderState.None)
+            throw new InvalidOperationException("Invalid command for state!");
+
+        if (command.Serving == Serving.paperbag && !command.Table.IsEmpty)
+            throw new InvalidOperationException("Invalid serving!");
+
+        _eventStore.Save(_id, new[]
+        {
+            new MealOrdered
+            {
+                Table = command.Table,
+                Serving = command.Serving.ToString(),
+                OrderNumber = orderNumberGenerator.GetNext(),
+                Items = command.Items.Select(i => new Item
+                {
+                    Category = i.Category.ToString(),
+                    Count = i.Count,
+                    Name = i.Name
+                }).ToArray()
+            }
+        });
+    }
 
     public void ConfirmItemPrepared(ConfirmMealItemPreparedCommand command){}
 
